689eeb85e07b5bc894bea83e36cfe90e842594ec
zhen <zhenw064@gmail.com>
2024-08-18 13:56:32 +0800

    setup env

--- chapter1-build-environment/1.0-build-environment/scripts/run-docker.sh
+++ chapter1-build-environment/1.0-build-environment/scripts/run-docker.sh
@@ -5,7 +5,7 @@ docker run -it \
 	--gpus all \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /home/zhen/dev:/home/zhen/dev \
-	-p 8091:22 \
+	-p 8092:22 \
 	-e DISPLAY=:1 \
 	trt_starter:cuda11.4-cudnn8-tensorrt8.2_${1} \
 	fish
